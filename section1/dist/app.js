061609b1bc2451eab9580d11d64a0da1f668a78e
kingkazuma7 <your.nasionasio7@gmail.com>
2024-08-03 11:22:01 +0900

    7. typescript hello world

---
+++ section1/dist/app.js
@@ -0,0 +1,4 @@
+const message = 'ハロー・ワールド';
+const age = 23;
+console.log(`${message}, 私は${age}歳です`);
+//# sourceMappingURL=app.js.map
---
+++ section1/src/app.ts
@@ -0,0 +1,3 @@
+const message: string = 'ハロー・ワールド';
+const age: number = 23;
+console.log(`${message}, 私は${age}歳です`);
