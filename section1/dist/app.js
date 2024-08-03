bc8e8e5be5706995c87af527be9e6d924c34ca64
kingkazuma7 <your.nasionasio7@gmail.com>
2024-08-03 12:25:37 +0900

    8. string, number, boolean

--- section1/dist/app.js
+++ section1/dist/app.js
@@ -1,4 +1,5 @@
 const message = 'ハロー・ワールド';
 const age = 23;
-console.log(`${message}, 私は${age}歳です`);
+const isValid = true;
+console.log(`${message}, 私は${age}歳です, 真偽値は${isValid}`);
 //# sourceMappingURL=app.js.map
--- section1/src/app.ts
+++ section1/src/app.ts
@@ -1,3 +1,4 @@
 const message: string = 'ハロー・ワールド';
 const age: number = 23;
-console.log(`${message}, 私は${age}歳です`);
+const isValid: boolean = true;
+console.log(`${message}, 私は${age}歳です, 真偽値は${isValid}`);
