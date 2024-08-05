89bc25eafc2dbd16ab0d9b48bd8302e113326bc3
kingkazuma7 <your.nasionasio7@gmail.com>
2024-08-05 12:30:55 +0900

    mac book proからコンパイル確認

--- section1/dist/app.js
+++ section1/dist/app.js
@@ -1,5 +1,5 @@
 const message = 'ハロー・ワールド';
-const age = 23;
+const age = 123;
 const isValid = true;
 console.log(`${message}, 私は${age}歳です, 真偽値は${isValid}`);
 //# sourceMappingURL=app.js.map
--- section1/src/app.ts
+++ section1/src/app.ts
@@ -1,4 +1,4 @@
 const message: string = 'ハロー・ワールド';
-const age: number = 23;
+const age: number = 123;
 const isValid: boolean = true;
 console.log(`${message}, 私は${age}歳です, 真偽値は${isValid}`);
