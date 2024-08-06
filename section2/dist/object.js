5821d33229028ce5cbd37e8f181f7e23ac26c8e4
kingkazuma7 <your.nasionasio7@gmail.com>
2024-08-07 08:42:05 +0900

    sec02  15 obj

---
+++ section2/dist/object.js
@@ -0,0 +1,6 @@
+const objTest = {
+    name: '名無し',
+    age: 20
+};
+console.log(objTest.name, objTest.age);
+//# sourceMappingURL=object.js.map
---
+++ section2/src/object.ts
@@ -0,0 +1,9 @@
+const objTest: {
+  name: string,
+  age: number
+} = {
+  name: '名無し',
+  age: 20
+}
+
+console.log(objTest.name, objTest.age);
