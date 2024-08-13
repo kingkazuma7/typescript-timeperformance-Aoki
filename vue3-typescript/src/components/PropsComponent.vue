0f97fb1d8ac44a18bcf94146808c1378e917ca9a
kingkazuma7 <your.nasionasio7@gmail.com>
2024-08-13 21:50:53 +0900

    45 defineProps 親で設定した情報がpropsにわたり表示

---
+++ vue3-typescript/src/components/PropsComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+type Props = {
+  id: number,
+  name: string
+}
+const props = defineProps<Props>()
+console.log(props);
+
+</script>
+
+<template>
+  <h2>PropsComponent</h2>
+  id: {{ props.id }}
+  name: {{ props.name }}
+</template>
--- vue3-typescript/src/views/HomeView.vue
+++ vue3-typescript/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import ComputedComponent from '@/components/ComputedComponent.vue';
 import EventComponent from '@/components/EventComponent.vue';
+import PropsComponent from '@/components/PropsComponent.vue';
 import ReactiveComponent from '@/components/ReactiveComponent.vue';
 import RefComponent from '@/components/RefComponent.vue'
 </script>
@@ -11,6 +12,7 @@ import RefComponent from '@/components/RefComponent.vue'
     <!-- <RefComponent /> -->
     <!-- <ReactiveComponent /> -->
     <!-- <EventComponent /> -->
-    <ComputedComponent />
+    <!-- <ComputedComponent /> -->
+    <PropsComponent v-bind:id=1 name="hgoe"  />
   </main>
 </template>
