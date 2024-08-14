40b1bb56d73679de6482b7854b179f1b28bda056
kingkazuma7 <your.nasionasio7@gmail.com>
2024-08-14 13:00:25 +0900

    46. tsでarray,objectの際はPropTypeで型を付ける

---
+++ vue3-typescript/src/components/PropTypeComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+type Items = {
+  id: number;
+  name: string;
+}[];
+
+const props = defineProps({
+  items: Array as PropType<Items>,
+});
+console.log(props.items);
+</script>
+
+<template>
+  <h2>PropTypeComponent</h2>
+  <ul>
+    <li v-for="item in props.items" :key="item.id">id: {{ item.id }}, name:{{ item.name }}</li>
+  </ul>
+</template>
--- vue3-typescript/src/views/HomeView.vue
+++ vue3-typescript/src/views/HomeView.vue
@@ -2,8 +2,15 @@
 import ComputedComponent from '@/components/ComputedComponent.vue';
 import EventComponent from '@/components/EventComponent.vue';
 import PropsComponent from '@/components/PropsComponent.vue';
+import PropTypeComponent from '@/components/PropTypeComponent.vue';
 import ReactiveComponent from '@/components/ReactiveComponent.vue';
 import RefComponent from '@/components/RefComponent.vue'
+
+const propTypeTest = [
+  { id: 1, name: '商品1' },
+  { id: 2, name: '商品2' }
+]
+
 </script>
 
 
@@ -13,6 +20,7 @@ import RefComponent from '@/components/RefComponent.vue'
     <!-- <ReactiveComponent /> -->
     <!-- <EventComponent /> -->
     <!-- <ComputedComponent /> -->
-    <PropsComponent v-bind:id=1 name="hgoe"  />
+    <!-- <PropsComponent v-bind:id=1 name="hgoe"  /> -->
+    <PropTypeComponent :items=propTypeTest />
   </main>
 </template>
