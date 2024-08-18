c43bc0072664cb04f1d614f5e22d5fdc7a89240a
kingkazuma7 <your.nasionasio7@gmail.com>
2024-08-18 10:44:13 +0900

    18. キーと値のペアを持つオブジェクト

--- section2/dist/objectinArray.js
+++ section2/dist/objectinArray.js
@@ -3,25 +3,30 @@ const members = [
         id: 1,
         name: 'ゾロ',
         hobby: '剣道',
+        age: 21
     }, {
         id: 2,
         name: 'ルフィ',
         hobby: '冒険',
+        age: 19
     }, {
         id: 3,
         name: 'ナミ',
         hobby: '航海',
+        age: 20
     }, {
         id: 4,
         name: 'ウソップ',
         hobby: '発明',
+        age: 19
     }, {
         id: 5,
         name: 'サンジ',
         hobby: '料理',
+        age: 21
     }
 ];
 for (const member of members) {
-    console.log(`name: ${member.name}の趣味は${member.hobby}です`);
+    console.log(`name: ${member.name}、${member.age}歳の趣味は${member.hobby}です`);
 }
 //# sourceMappingURL=objectinArray.js.map
--- section2/src/objectinArray.ts
+++ section2/src/objectinArray.ts
@@ -4,30 +4,39 @@ type ObjectInArray1 = {
   hobby: string
 }[] // 配列
 
-const members : ObjectInArray1 = [
+type ObjectInArray2 = {
+  [key: string] : string | number  | boolean
+}[]
+
+const members : ObjectInArray2 = [
   {
     id: 1,
     name: 'ゾロ',
     hobby: '剣道',
+    age: 21
   }, {
     id: 2,
     name: 'ルフィ',
     hobby: '冒険',
+    age: 19
   }, {
     id: 3,
     name: 'ナミ',
     hobby: '航海',
+    age: 20
   }, {
     id: 4,
     name: 'ウソップ',
     hobby: '発明',
+    age: 19
   }, {
     id: 5,
     name: 'サンジ',
     hobby: '料理',
+    age: 21
   }
 ]
 
 for (const member of members) {
-  console.log(`name: ${member.name}の趣味は${member.hobby}です`);
+  console.log(`name: ${member.name}、${member.age}歳の趣味は${member.hobby}です`);
 }
