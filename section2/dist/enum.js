668004b76b2913d1362cd598cd85c366d7d9ab86
kingkazuma7 <your.nasionasio7@gmail.com>
2024-08-19 07:08:56 +0900

    21. enum 列挙型 即時関数になるjsで

---
+++ section2/dist/enum.js
@@ -0,0 +1,10 @@
+var SIZEnum;
+(function (SIZEnum) {
+    SIZEnum[SIZEnum["small"] = 0] = "small";
+    SIZEnum[SIZEnum["medium"] = 1] = "medium";
+    SIZEnum[SIZEnum["large"] = 2] = "large";
+})(SIZEnum || (SIZEnum = {}));
+console.log(SIZEnum.large);
+console.log(SIZEnum[0]);
+console.log(SIZEnum[10]);
+//# sourceMappingURL=enum.js.map
---
+++ section2/src/enum.ts
@@ -0,0 +1,10 @@
+enum SIZEnum {
+  'small',
+  'medium',
+  'large'
+}
+console.log(SIZEnum.large);
+
+console.log(SIZEnum[0]);
+
+console.log(SIZEnum[10]);
